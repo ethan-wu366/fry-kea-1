cd43161b7c67a39b2344085c634bea9a44a19659
Ethan Wu <ewu000@berkeley.edu>
2021-09-07 11:04:16 -0700

    landing

---
+++ fry-kea/src/components/Landing.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import $ from 'jquery';
+
+import '../css/Landing.css';
+
+const Landing = () => {
+	$(document).ready(function () {
+		/* Check the location of each desired element */
+		$('.fade-in').each(async function (i) {
+			var top_of_object = $(this).offset().top;
+			var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+			/* If the object is completely visible in the window, fade it it */
+			if (bottom_of_window > top_of_object) {
+				$(this).animate({ opacity: '1' }, 550);
+			}
+		});
+		/* Every time the window is scrolled */
+		$(window).scroll(async function () {
+			/* Check the location of each desired element */
+			$('.fade-in').each(function (i) {
+				var top_of_object = $(this).offset().top;
+				var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+				/* If the object is completely visible in the window, fade it it */
+				if (bottom_of_window > top_of_object) {
+					$(this).animate({ opacity: '1' }, 350);
+				}
+			});
+		});
+	});
+	return (
+        <p>Hello</p>
+        );
+};
+
+export default Landing;
